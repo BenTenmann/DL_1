cc8cd6582f913253d74f66c8e31e4acfcfce5bcd
Benjamin Tenmann <benjamintenmann@BennyT2020.local>
2020-12-06 16:58:37 +0000

    perceptron update

--- perceptron.py
+++ perceptron.py
@@ -9,13 +9,13 @@ elements (x_i) to be "positive" or "negative".
 @author: benjamintenmann
 """
 import numpy as np
-import random
-from scipy import stats
-from math import exp
-import multiprocessing as mp
+import math
 import matplotlib.pyplot as plt
-import time 
 
+def printDone(i, ep):
+    if (i+1) % (ep*0.1) == 0:
+        frac = int((i+1)/(ep*0.1))
+        print('[*'+('*'*frac)+(' '*(10-frac))+']')
 
 
 def fMSE(prediction, real):
@@ -25,19 +25,6 @@ def fMSE(prediction, real):
         SE = list(map(lambda x, y: (x-y)**2, prediction, real))
         MSE = sum(SE)/len(SE)
         return MSE
-
-
-def compute(x, w, i, step):
-    logistic = lambda r : 1/(1+exp(-r))
-    d_logistic = lambda x_i, w_i : (x_i * exp(-(x_i * w_i)))/(1-exp(-(x_i * w_i)))**2
-    gradient_descent = lambda step, t, y, g: (1/step) * (t-y) * g
-    
-    y = logistic(x[:-1].dot(w))
-    t = x[-1]
-    g = d_logistic(x[i], w[i])
-    
-    w[i] = w[i] + gradient_descent(step, t, y, g)
-    return w[i]
 
 
 def epoch(mtX, w):
@@ -52,26 +39,23 @@ def epoch(mtX, w):
     w : Updated weights vector.
 
     """
-    print('epoch commences...')
-    start = time.time()
+    exp = math.exp
     X = mtX.copy()
     np.random.shuffle(X)
+    
+    logistic = lambda r : 1/(1+exp(-r))
+    d_logistic = lambda x_i, w_i : (x_i * exp(-(x_i * w_i)))/(1-exp(-(x_i * w_i)))**2
+    gradient_descent = lambda step, t, y, g: (1/step) * (t-y) * g
     step = 1
-    ax = plt.subplot(1,1,1)
-    prediction = 1/(1+exp(-(X[0, :-1].dot(w))))
-    real = X[0,-1]
-    ax.scatter(step, fMSE(prediction, real))
     for x in X:
-        args = [*zip([x]*10, [w]*10, list(range(10)), [step]*10)]
-        pool = mp.Pool(10)
-        w = np.array(list(pool.starmap(compute, args)))
-        pool.close()
+        for i, x_i in enumerate(x[:-1]):
+            y = logistic(x[:-1].dot(w))
+            t = x[-1]
+            g = d_logistic(x[i], w[i])
+            
+            w[i] = w[i] + gradient_descent(step, t, y, g)
+            
         step += 1
-        prediction = 1/(1+exp(-(x[:-1].dot(w))))
-        real = x[-1]
-        ax.plot(step, fMSE(prediction, real))
-    end = time.time()
-    print('completed! t:', end-start,'\n')
     return w
     
 
@@ -89,19 +73,22 @@ class perceptron:
         self.data = A
         
     def learn(self, inds, epochs = 1000):
-        print('neuron starts learning...\n\n')
+        print('neuron starts learning...\n')
         B = self.data
         X = B[inds, :]
         
         W = w_n = np.ones(10)
         n_epochs = range(epochs)
+        print('progress:\n[*          ]')
         for ep in n_epochs:
+            printDone(ep, epochs)
             w_n = epoch(X, w_n)
             W = np.vstack([W, w_n])
-        print('done!\n\n')
+        print('\ndone!\n\n')
         return W # return weight matrix --- rows are different epochs
         
     def test(self, inds, w):
+        exp = math.exp
         print('neuron starts testing...\n')
         B = self.data
         r_ind = np.arange(1, B.shape[0])
@@ -109,15 +96,15 @@ class perceptron:
         Y = B[ind, :].copy()
         prediction = list(map(lambda x, w : 1/(1+exp(-(x.dot(w)))), Y[:,:-1], [w for x in range(Y.shape[0])]))
         real = Y[:, -1]
-        print('prediction:', prediction[0:10])
-        print('real:', real[0:10])
+        #print('prediction:', prediction[0:10])
+        #print('real:', real[0:10])
         MSE = fMSE(prediction, real)
         print('done!')
         return MSE
         
 if __name__ == '__main__':    
     # creating random data
-    n = 1000000
+    n = 100000
     B = np.random.randint(0, 2, size=(n,10))
     B = np.where(B == 0, -1, B)
     A = np.zeros((n, 11))
@@ -126,12 +113,13 @@ if __name__ == '__main__':
     inds = np.random.randint(0, A.shape[0], size=int(A.shape[0] * 0.7))
     
     # initialise perceptron object
+    p = 1000
     neuron = perceptron(A)
-    weights = neuron.learn(inds, epochs=10)
+    weights = neuron.learn(inds, epochs=p)
     errors = [neuron.test(inds, m) for m in weights]
-    eps = list(range(1, 12))
+    eps = list(range(1, p+2))
     
-    plt.scatter(eps, errors)
+    plt.plot(eps, errors)
     plt.show()
 
 
