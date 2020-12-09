de363d251c1dac99cac857791c13f87b3a60273b
Benjamin Tenmann <benjamintenmann@BennyT2020.local>
2020-12-09 14:45:02 +0000

    added MLP

---
+++ MLP/MLP.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+MLP: Multi-layer perceptron
+A MLP to solve non-linear problems
+
+@author: benjamintenmann
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+def printDone(i, ep):
+    if (i+1) % (ep*0.1) == 0:
+        frac = int((i+1)/(ep*0.1))
+        print('[*'+('*'*frac)+(' '*(10-frac))+']')
+
+def weighted_sum(x, W):
+    return x.dot(W)
+
+def activation(z):
+    exp = math.exp
+    y = 1/(1+np.array([exp(-n) for n in z]))
+    return y
+
+def d_activation(z):
+    exp = math.exp
+    derivative = np.array([exp(-m) for m in z])/(1+np.array([exp(-n) for n in z]))**2
+    return derivative
+
+def E_r(Y, M):
+    y_hat = np.array([forprop(y, M[0], M[1])[1] for y in Y])
+    print('prediction:', y_hat, '\nreal:', Y, '\n\n', sep='\n')
+    err = lambda t, y: 0.5 * sum((t-y)**2)
+    error = list(map(err, Y, y_hat))
+    MSE = sum(error)/len(error)
+    return MSE
+
+def delta(z, error):
+    derivative = d_activation(z)
+    return derivative * error
+
+def forprop(x, *M):
+    z = x
+    res = []
+    for W in M:
+        z = activation(weighted_sum(z, W))
+        res.append(z)
+    return res
+
+def backprop(x, step, M):
+    z_j, z_k = forprop(x, M[0], M[1])
+    error = x - z_k
+    delta_k = delta(weighted_sum(z_j, M[1]), error)
+    W_2 = M[1] + ((1/step) * np.outer(z_j, delta_k))
+    
+    delta_j = delta(weighted_sum(x, M[0]), weighted_sum(delta_k, W_2.T))
+    W_1 = M[0] + ((1/step) * np.outer(x, delta_j))
+    
+    return W_1, W_2
+
+def epoch(mtX, M):
+    X = mtX.copy()
+    np.random.shuffle(X)
+    
+    step = 10
+    for x in X:
+        M = backprop(x, step, M)
+        step += 10
+    return M
+
+class MLP:
+    
+    def __init__(self, wshape = [(8,3), (3,8)]):
+        self.data = np.identity(8)
+        self.weights = [np.ones(m) for m in wshape]
+        
+    def train(self, inds, epochs=1000):
+        print('neural net begins to train...\n')
+        B = self.data
+        X = B[inds, :]
+        M = self.weights
+        ls = [M]
+        print('progress:')
+        for r in range(epochs):
+            M = epoch(X, M)
+            ls.append(M)
+            printDone(r, epochs)
+        print('\ndone!\n\n')
+        return ls
+    
+    def test(self, inds, M):
+        print('testing epochs...')
+        B = self.data
+        r_ind = np.arange(0, 8)
+        ind = r_ind[np.in1d(r_ind, inds, invert=True)]
+        Y = B[np.arange(0,8), :]
+        errs = [E_r(Y, m) for m in M]
+        print('\ndone!\n\n')
+        return errs
+        
+
+        
+        
+if __name__=='__main__':
+    n = 200
+    neural_net = MLP()
+    inds = np.random.choice(np.arange(0,8), size=6, replace=False)
+    M = neural_net.train(np.arange(0,8), epochs = n)
+    errors = neural_net.test(inds, M)
+    eps = list(range(n+1))
+    
+    plt.plot(eps, errors)
+    plt.show()
+    
+    
+    
+    
--- data_vis.R
+++ data_vis.R
@@ -9,3 +9,15 @@ read.csv("/Users/benjamintenmann/Desktop/CompBio/Assignments/DL_1/perceptron/ep_
   theme_minimal()+
   ylab("mean-squared error (MSE)")+
   theme(axis.line = element_line(), axis.ticks = element_line())
+
+data.frame(x = c(1, 1, -1, -1),
+           y = c(1, -1, 1, -1),
+           parity = c('+', '-', '-', '+')) %>%
+  ggplot(., aes(x=x, y=y, color=parity))+
+  geom_point(size = 4)+
+  xlab(expression(paste('x'[1], sep='')))+
+  ylab(expression(paste('x'[2], sep='')))+
+  theme_minimal()+
+  geom_hline(yintercept = 0)+
+  geom_vline(xintercept = 0)
+  #theme(axis.line = element_line())
